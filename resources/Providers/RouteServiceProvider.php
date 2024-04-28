3398f8f518e6f791dcace9e2f94aa768bff422c2
digital-brew <rafal+github@digitalbrew.io>
2024-04-28 11:19:21 +0100

    Initial commit

---
+++ resources/Providers/RouteServiceProvider.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace DigitalBrew\AdminBrewer\Providers;
+
+use Themosis\Core\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Themosis\Support\Facades\Route;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    /**
+     * Controller namespace for plugin routes.
+     *
+     * @var string
+     */
+    protected $namespace = 'DigitalBrew\AdminBrewer\Controllers';
+
+    public function boot()
+    {
+        parent::boot();
+    }
+
+    /**
+     * Load plugin routes.
+     */
+    public function map()
+    {
+      $plugin_dev_path = '/Volumes/Work/Code/Wordpress/plugins/';
+      if (env('APP_ENV') === 'local') {
+        $pluginName = ltrim(
+          str_replace($plugin_dev_path, '', realpath(__DIR__.'/../../')),
+          '\/'
+        );
+      } else {
+        $pluginName = ltrim(
+          str_replace(plugins_path(), '', realpath(__DIR__.'/../../')),
+          '\/'
+        );
+      }
+
+        Route::middleware('web')
+            ->namespace($this->namespace)
+            ->group(plugins_path($pluginName.'/routes.php'));
+    }
+}
